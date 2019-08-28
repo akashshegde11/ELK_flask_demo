60ef9c2aff88c43dbe88dcac2c88a03ad9e5e470
akashshegde11 <44206999+akashshegde11@users.noreply.github.com>
2019-08-28 14:28:26 +0530

    Added source for Flask app.

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+# ELK Demo - Flask
+
+from flask import Flask
+from elasticapm.contrib.flask import ElasticAPM
+app = Flask(__name__)
+app.config['ELASTIC_APM'] = {
+    'SERVICE_NAME': 'my_service'
+}
+apm = ElasticAPM(app, logging=True)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, world!'
